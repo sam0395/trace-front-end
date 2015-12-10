15d2375240e4146821dc4e097344984f8e392be1
sam0395 <sam0395@yahoo.com>
2015-12-10 13:25:26 -0500

    the biography section of the top view is now set up

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 
 <?php
-	include "header.php";
-	include "nav-bar.php";
+include "header.php";
+include "profile-nav-bar.php";
+
+$bio = "This is an example biography. Users can set their own biographies when setting up their accounts. <br /> -Sam";
 ?> 
 
 <div class="container">
@@ -9,68 +11,91 @@
 
     <div class="col-lg-3 col-sm-6">
 
-            <div class="card hovercard">
-                <div class="cardheader" style="background: url('images/header4.jpg');  background-size: cover;">
-
-                </div>
-                <div id="profile_pic"  class="avatar">
-                    <img alt="" src="images/sam.jpg">
-                </div>
-                <div class="info">
-                    <div class="title">
-                        <a target="_blank" href="http://scripteden.com/">Sam Belcastro</a>
-                    </div>
-                    <div class="desc">Treasurer</div>
-                    <div class="desc">Data Analyst</div>
-                    <div class="desc">Computer Science</div>
-                </div>
-                <div class="bottom">
-                    <div id="bio_button" class="btn btn-primary btn-twitter btn-sm">B</div>
-                    <div id="stats_button" class="btn btn-primary btn-twitter btn-sm">S</div>
-                    <div id="news_button" class="btn btn-primary btn-twitter btn-sm">N</div>
-                    <div id="bio_button" class="btn btn-primary btn-twitter btn-sm"></div>
-                </div>
-            </div>
+      <div class="card hovercard">
+        <div class="cardheader" style="background: url('images/header4.jpg');  background-size: cover;">
 
         </div>
-
-            <div class="col-lg-9 col-sm-6">
-
-            <div id="special" class="space">
-               <div id="text">Click B, S, or N to test!</div>
-            </div>
-
+        <div id="profile_pic"  class="avatar">
+          <img alt="" src="images/sam.jpg">
         </div>
+        <div class="info">
+          <div class="title">
+            <a target="_blank" href="http://scripteden.com/">Sam Belcastro</a>
+          </div>
+          <div class="desc">Treasurer</div>
+          <div class="desc">Data Analyst</div>
+          <div class="desc">Computer Science</div>
+        </div>
+        <div class="bottom">
+          <div id="bio_button" class="btn btn-primary btn-twitter btn-sm">B</div>
+          <div id="stats_button" class="btn btn-primary btn-twitter btn-sm">S</div>
+          <div id="news_button" class="btn btn-primary btn-twitter btn-sm">N</div>
+          <div id="bio_button" class="btn btn-primary btn-twitter btn-sm"></div>
+        </div>
+      </div>
 
 
-	<script language="javascript" type="text/javascript" src="dist/js/bootstrap.js"></script>
-	<script language="javascript" type="text/javascript" src="dist/js/jquery.js"></script>
+    </div>
+ 
 
-  <script language="javascript" type="text/javascript">
+    <div class="col-lg-9 col-sm-6">
+
+      <div id="special" class="space">
+                        
+      </div>   
+    </div>
+ </div> 
+
+
+   <script language="javascript" type="text/javascript" src="dist/js/jquery.js"></script>
+   <script language="javascript" type="text/javascript" src="dist/js/bootstrap.js"></script>
+   
+
+   <script language="javascript" type="text/javascript">
+
+   var selection;
+   var html;
+
    $(document).ready(function(){
+
     $("#bio_button").click(function(){
-        $('#text').text("Biography");
-    });
-     $("#stats_button").click(function(){
-      
-      $('#text').text("Statistics");
-        
-        
+      selection = "bio";
+      html = generateHtml(selection);
+      $('#special').html(html);
+      $("#bio").fadeIn(2000);
     });
 
-     $("#news_button").click(function(){
-        $('#text').text("Headline Club News");
+    $("#stats_button").click(function(){
+      $('#text').html("<br /><center>Statistics</center>");
     });
-});
-  </script>
 
-</body>
+    $("#news_button").click(function(){
+      $('#text').text(html);
+    });
+  });
+
+   function generateHtml(user_choice)
+   {
+      if(user_choice == "bio")
+      {
+      html = '<div id="bio" class="bio col-lg-12"><?php echo $bio;?></div> ';
+      }
+
+      return html;
+   }
 
 
-<?php
-  include "footer.php";
-?>
 
-</html>
+
+   </script>
+
+ </body>
+
+
+ <?php
+ include "footer.php";
+ ?>
+
+ </html>
 
 
